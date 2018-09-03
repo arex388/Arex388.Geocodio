20ec47a4abce3cebbdc62dbca480550c7647eee9
arex388 <31530399+arex388@users.noreply.github.com>
2018-09-03 13:17:11 -0700

    Converted to .NET Standard 1.1 to publish as a NuGet package.

--- Models/GeocodeRequest.cs
+++ Models/GeocodeRequest.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
 
 namespace Arex388.Geocodio {
 	public sealed class GeocodeRequest :
@@ -9,7 +9,7 @@ namespace Arex388.Geocodio {
 		public override string Endpoint {
 			get {
 				var parameters = new HashSet<string> {
-					$"q={HttpUtility.UrlEncode(Address)}"
+					$"q={WebUtility.UrlEncode(Address)}"
 				};
 
 				if (Fields.Any()) {
